a87dd815814d98edb479d32862849e3adb1b9daa
Noah Alemayehu <noahlem21@gmail.com>
2024-01-17 02:44:56 -0800

    placeUpdate

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,11 +6,12 @@ from models.base_model import Base
 from models.city import City
 from models.state import State
 from models.user import User
+from models.place import Place
 from os import getenv
 
 
 classes = {"State": State, "City": City,
-           "User": User}
+           "User": User, "Place": Place}
 
 
 class DBStorage:
